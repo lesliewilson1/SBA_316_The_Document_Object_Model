4ec96bcfcfd9fb60664a2ad957e93098f94d09b7
Leslie <wilson.leslie2021@gmail.com>
2025-09-18 00:56:01 -0400

    Retention list

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
-//Creating the user input box
 const form = document.getElementById("form");
+const historyList = document.getElementById("historyList");
+
+//Creating the user input box
 
 const input = document.createElement('input');
 input.type = 'text';
@@ -8,8 +10,31 @@ input.placeholder = 'Type lyrics here';
 input.style.width = '300px';
 input.style.height = '25px';
 input.style.fontSize = '18px';
-input.style.marginTop = '50px';
+input.style.marginTop = '40px';
 form.appendChild(input);
 
-// Input function
+//Create button
+
+const button = document.createElement('button');
+button.textContent = "Enter";
+form.appendChild(button);
+
+//Display history list
+
+button.addEventListener('click', function handleClick(e){
+    e.preventDefault();
+    const userText = input.value.trim();
+if (userText !== '' ){
+    const listItem = document.createElement('li');
+    listItem.textContent = userText;
+    historyList.appendChild(listItem);
+    input.value = '';
+}
+
+});
+
+
+
+
+
 
