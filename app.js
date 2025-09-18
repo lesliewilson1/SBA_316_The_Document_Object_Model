af5fee3ed8353cbe57be80704e43eacb03755b00
Leslie <wilson.leslie2021@gmail.com>
2025-09-17 23:17:25 -0400

    Updated style

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const form = document.getElementById("form");
 
 const input = document.createElement('input');
 input.type = 'text';
-input.placeholder = 'Type your words here';
-
+input.placeholder = 'Type lyrics here';
+input.style.width = '300px';
+input.style.height = '25px';
+input.style.fontSize = '18px';
+input.style.marginTop = '50px';
 form.appendChild(input);
 
 // Input function
