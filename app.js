afd1c1b7f710b2d4b4ad008f1a308c5e3bd9d91f
Leslie <wilson.leslie2021@gmail.com>
2025-09-17 20:01:09 -0400

    Updating Repo 2.0

---
+++ app.js
@@ -0,0 +1,12 @@
+
+//Creating the user input box
+const form = document.getElementById("form");
+
+const input = document.createElement('input');
+input.type = 'text';
+input.placeholder = 'Type your words here';
+
+form.appendChild(input);
+
+// Input function
+
