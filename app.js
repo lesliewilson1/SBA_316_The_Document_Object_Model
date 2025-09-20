d8039fd12552cc8f8270acf853231b68173027b5
Leslie <wilson.leslie2021@gmail.com>
2025-09-19 23:56:19 -0400

    Update 3.0

--- app.js
+++ app.js
@@ -1,62 +1,106 @@
 
 const form = document.getElementById("form");
 const historyList = document.getElementById("historyList");
+const userInputs = [];
 
 //Creating the user input box
 
 const input = document.createElement('input');
 input.type = 'text';
-input.placeholder = 'Type lyrics here';
+input.placeholder = 'Type phrase here';
 input.style.width = '300px';
 input.style.height = '25px';
 input.style.fontSize = '18px';
 input.style.marginTop = '40px';
 form.appendChild(input);
 
-//Create button
+//Create and attach button
 
 const button = document.createElement('button');
 button.style.cursor = 'pointer';
 button.style.fontSize = '18px';
 button.textContent = "Enter";
-form.appendChild(button);
+form.appendChild(button); 
 
-//Display history list && add rhyme button
+
+
+// Prevent additional clicks and duplicates
 
 button.addEventListener('click', function handleClick(e){
     e.preventDefault();
     const userText = input.value.trim();
 if (userText !== '' ){
+    let duplicate = false;
+    for (let i = 0; i < userInputs.length; i++) {
+        if (userInputs[i] === userText) {
+        duplicate = true;
+    }
+        
+}
+    if(duplicate) {
+        alert("Same phrase entered!");
+
+    }
+
+    userInputs.push(userText);
+    console.log(userInputs);
+
     const listItem = document.createElement('li');
     listItem.textContent = userText;
     historyList.appendChild(listItem);
     input.value = '';
 
+}
+});
+
+
+
+// Add new rhyme button
     const rhymeButton = document.createElement('button');
     rhymeButton.textContent = 'Rhyme';
     rhymeButton.id = 'rhymeButtonStyle';
     rhymeButton.classList.add('rhyme-button-style');
     document.querySelector('.homepage').appendChild(rhymeButton);
-
+    
     rhymeButton.addEventListener('click', function handleClick(e) {
         e.preventDefault();
-        fetch(`https://api.api-ninjas.com/v1/rhyme?word=${userText}`, {
-            method: 'GET',
-            headers: {
-                'X-Api-Key': 'SyyRloP1rR4F3Ke5sBEXDw==gm9quogQtyLerbJ4'
-            }
-        })
+        alert("🔒For subscribers only🔒");
 
-    });
-
-
-
-
-    
+     let inputCount = 0;
+        if (inputCount >= 100000){
+            rhymeButton.disabled = false;
+        }
 }
+);
+
+//Clone 
+
+const template = document.getElementById9=('template');
+const clone = template.content.cloneNode(true);
+const listItem = clone.querySelector('.history-item');
+listItem.textContent = userText;
+
+historyList.appendChild(clone);
+
+// Attempting fetch API data
+        // fetch(`https://api.api-ninjas.com/v1/rhyme?word=${userText}`, {
+        //     method: 'GET',
+        //     headers: {
+        //         'X-Api-Key': 'SyyRloP1rR4F3Ke5sBEXDw==gm9quogQtyLerbJ4'
+        //     }
+        // })
+        // .then(response => response.json())
+        // .then(data => {
+        //     console.log("Rhymes:", data);
+
+        // })
+        // .catch(error => {
+        //     console.error("Error", error);
+        // })
 
 
-});
+
+
 
 
 
